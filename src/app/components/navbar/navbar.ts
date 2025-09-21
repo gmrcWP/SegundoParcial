da32fb8e5bd5333a961a017e167a05de639af4e3
gmrcWP <jorgesick.roch@gmail.com>
2025-09-21 19:14:19 -0400

    feat(router+a11y): routes and basic accessibility improvements

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  private router = inject(Router);
 
+  get isOnUIRoute(): boolean {
+    return this.router.url === '/ui';
+  }
 }
