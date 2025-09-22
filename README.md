1e1ec2b9004de109cf9debad12495fc9e411133f
gmrcWP <jorgesick.roch@gmail.com>
2025-09-21 22:21:13 -0400

    feat(pwa): offline-ready home with precaching

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ TAILWINDCSS
 Fuente: https://tailwindflex.com/@limaa-m/simple-landing-page
 Licencia:
 Copyright (c) 2023 - Present, Designed & Developed by [Themefisher](https://tailwindflex.com/)
+
+## Prueba Offline (PWA)
+1. Se configuró `ngsw-config.json` con precaching de `index.html`, `*.css`, `*.js` y recursos estáticos clave.
+2. Se compiló la aplicación en modo producción:
+3. Se sirvió con un servidor local: npx http-server -p 8080 -c-1 dist/segundoParcial/browser. En Devtools -> Application -> Service Workers:
+- Se activo el modo Offline
+- Home se cargo correctamente sin conexion
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { App } from './app/app';
 import { provideServiceWorker } from '@angular/service-worker';
 import { isDevMode } from '@angular/core';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then((registration) => {
+      console.log('Service Worker registrado: ', registration);
+    }).catch((error) => {
+      console.error('Error en el registro del Service Worker: ', error);
+    });
+  });
+}
+
 bootstrapApplication(App, {
   providers: [
-    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+    ...appConfig.providers
   ],
-});
+}).catch((error) => console.error('Error al inicializar la app: ', error));
