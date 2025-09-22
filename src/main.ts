6ebc143e545f3d51035e1d052a59cc567d7ea323
gmrcWP <jorgesick.roch@gmail.com>
2025-09-22 15:03:45 -0400

    Sol. errores

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideServiceWorker } from '@angular/service-worker';
-import { isDevMode } from '@angular/core';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/ngsw-worker.js').then((registration) => {
-      console.log('Service Worker registrado: ', registration);
-    }).catch((error) => {
-      console.error('Error en el registro del Service Worker: ', error);
-    });
-  });
-}
-
-bootstrapApplication(App, {
-  providers: [
-    ...appConfig.providers
-  ],
-}).catch((error) => console.error('Error al inicializar la app: ', error));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
