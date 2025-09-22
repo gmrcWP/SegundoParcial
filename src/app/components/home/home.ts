cdd45b9a625185f9f300134cbd65a831766b19c1
gmrcWP <jorgesick.roch@gmail.com>
2025-09-21 22:52:45 -0400

    feat(noti): local notification permission and demo buttons

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Hero } from '../hero/hero';
 import { MainFeatures } from '../main-features/main-features';
@@ -15,6 +15,32 @@ import { Footer } from '../footer/footer';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+
+export class Home implements OnInit {
+  ngOnInit() {
+    const allowBtn = document.getElementById('allowBtn');
+    const testBtn = document.getElementById('testBtn');
+
+    allowBtn?.addEventListener('click', () => {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          alert('Permiso concedido');
+        } else {
+          alert('Permiso denegado');
+        }
+      });
+    });
+
+    testBtn?.addEventListener('click', () => {
+      if (Notification.permission === 'granted') {
+        new Notification('Prueba', {
+          body: 'Esta es una notificación de prueba',
+          icon: '/icons/icon-144x144.png',
+        });
+      } else {
+        alert('Primero debes permitir notificaciones');
+      }
+    });
+  }
 
 }
